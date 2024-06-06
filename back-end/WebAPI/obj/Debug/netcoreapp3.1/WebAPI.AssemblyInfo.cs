8180d4280ca0238a079daf7380cada45a3d3238a
Babu Somasundaram <bso4cob@bosch.com>
2024-06-06 15:00:57 +0530

    uploading files

--- back-end/WebAPI/obj/Debug/netcoreapp3.1/WebAPI.AssemblyInfo.cs
+++ back-end/WebAPI/obj/Debug/netcoreapp3.1/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd598502ff59205d7cf54ff1eb8517f9c5ebdf85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8085b8d739410e600f8aacdbd43a696e882ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
